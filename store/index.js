1ae45f603dcd938a90961519f4654bc0427680c7
toshi <reactoshi@gmail.com>
2019-04-21 16:22:29 +0900

    show card number when click it

--- store/index.js
+++ store/index.js
@@ -17,14 +17,21 @@ export const mutations = {
   openCard(state, { roomId, src }) {
     state.rooms = state.rooms.map((room, i) => {
       if (i === roomId) {
+        let cardNum = src.replace(/[^0-9]/g, '')
+        if (cardNum <= 13) {
+          // cardNum
+        } else if (cardNum >= 14 && cardNum <= 26) {
+          cardNum = cardNum % 13
+        } else if (cardNum >= 27 && cardNum <= 39) {
+          cardNum = cardNum % 26
+        } else if (cardNum >= 40 && cardNum <= 52) {
+          cardNum = cardNum % 39
+        }
+        console.log(cardNum)
         return {
           cards: room.cards.map((card) => {
             return card.src === src
-              ? {
-                ...card,
-                opened: true
-              }
-              : card
+              ? { ...card, opened: true } : card
           })
         }
       } else {
